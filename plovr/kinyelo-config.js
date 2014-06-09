fdf114d15043dd472a5f57fa15dec6883425cca3
Michael Hill <asiral@gmail.com>
2014-06-09 19:45:00 +0400

    plovr config

---
+++ plovr/kinyelo-config.js
@@ -0,0 +1,7 @@
+{
+	id: "kinyelo",
+	inputs: "../public/js/kinyelo/rte.js",
+	paths: ["../public/js/kinyelo/","../public/templates/","../public/js/soy/"],
+	closure-library: "../public/js/goog/",
+    externs: "../public/js/vendor/jquery-1.9.externs.js"
+}
