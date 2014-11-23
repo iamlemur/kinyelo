0587107e1d2063ed575c2fa7e6f5a28702457661
Michael Hill <asiral@gmail.com>
2014-11-23 13:45:48 -0500

    prepare for deployment

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -87,7 +87,13 @@
 <script src="/js/vendor/waypoints-sticky.min.js"></script>
 <script src="/js/vendor/icheck.min.js"></script>
 <!--script>window.jQuery || document.write('<script src="/js/vendor/jquery-1.10.2.min.js"><\/script>')</script-->
-<script src="http://kinyelo.com:9810/compile?id=kinyelo&mode=raw&level=verbose" type="text/javascript"></script>
+
+@if(App::environment('dev'))
+    <script src="http://kinyelo.com:9810/compile?id=kinyelo&mode=raw&level=verbose" type="text/javascript"></script>
+@else
+    <script src="/js/compiled.js" type="text/javascript"></script>
+@endif
+
 <!--script src="http://kinyelo.com/js/compiled.js" type="text/javascript"></script-->
 <script src="/js/scripts.js" type="text/javascript"></script>
 </body>
